1ce905dde3d0655c3b53cf63a5ade804b9ecbf14
Throwback74 <corey.slade@gmail.com>
2018-06-18 09:14:09 -0700

    morning desktop update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,27 +7,27 @@ $(document).ready(function() {
     var comNum = function () {
         console.log(Math.floor(Math.random() * (120-19)) + 19);
     }
-    
+    comNum()
     var SaphNum = function () {
         console.log(Math.floor(Math.random() * (12-1)) + 1);
     }
-
+    SaphNum()
     var RubyNum = function () {
         console.log(Math.floor(Math.random() * (12-1)) + 1);
     }
-
+    RubyNum()
     var EmerNum = function () {
         console.log(Math.floor(Math.random() * (12-1)) + 1);
     }
-
+    EmerNum()
     var DiaNum = function () {
         console.log(Math.floor(Math.random() * (12-1)) + 1);
     }
-
+    DiaNum()
     var reset = function () {
         $("#cNumber").html(0);
         $("#scoreTotal").html(0);
     }
-
+    
 
 })
